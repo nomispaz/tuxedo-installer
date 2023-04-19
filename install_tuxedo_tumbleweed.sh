dd644140e92630c71c738b90274003e9e7a6e802
nomispaz <111981114+nomispaz@users.noreply.github.com>
2023-04-19 21:45:22 +0200

    Add files via upload

---
+++ install_tuxedo_tumbleweed.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# build tuxedo-keyboard
+
+# install prerequisites
+sudo zypper install make gcc kernel-devel dkms
+# clone current git
+git clone https://github.com/tuxedocomputers/tuxedo-keyboard.git ~/git_clones/tuxedo-keyboard
+cd ~/git_clones/tuxedo-keyboard/
+
+# create rpm package with build-in package command
+git checkout release
+
+patch ~/git_clones/tuxedo-keyboard/src_pkg/rpm_pkg.spec ~/git_clones/tuxedo-installer/rpm_pkg.patch
+
+sudo make package-rpm
+
+# copy to home of current user
+cp tuxedo-keyboard*.rpm ~/
+
+# goto home of current user
+cd
+
+# remove build-dir
+sudo rm -R ~/git_clones/tuxedo-keyboard/
+
+##########################################################################
+
+# build tuxedo-control-center
+
+# install prerequisites
+sudo zypper install git gcc gcc-c++ make nodejs npm systemd-devel
+# clone current git
+git clone https://github.com/tuxedocomputers/tuxedo-control-center ~/git_clones/tuxedo-contol-center
+cd ~/git_clones/tuxedo-contol-center
+
+# build program files
+npm install
+
+# set environment to eliminate ERR_OSSL_EVP_UNSUPPORTED error (see https://stackoverflow.com/questions/69394632/webpack-build-failing-with-err-ossl-evp-unsupported)
+export NODE_OPTIONS=--openssl-legacy-provider
+
+# create rpm package
+npm run pack-prod rpm
+
+# copy to home of current user
+cp ~/git_clones/tuxedo-contol-center/dist/packages/tuxedo-control-center*.rpm ~/
+
+# goto home of current user
+cd
+
+# remove build-dir
+sudo rm -R ~/git_clones/tuxedo-contol-center
+
+############################################################################
+
+# install packages
+# fedora
+sudo zypper install ./tuxedo-control-center_2.0.2.rpm ./tuxedo-keyboard-3.2.1-1.noarch.rpm
